10954b7d8be1867f3d1bd6d30c223280f55655de
Vladimi2boom <97739181+Vladimi2boom@users.noreply.github.com>
2023-12-02 17:13:47 +0200

    Add files via upload

--- update_check.py
+++ update_check.py
@@ -15,18 +15,7 @@ def get_str_from_file(filename):
 			ret = data_file.read().strip("\n")
 		data_file.close()
 		return ret
-		
-def get_current_version():
-	filename = "/boot/dietpi/.version"
-	ver = ""
-	if os.path.exists(filename):
-		for line in open(filename, "r"):
-			if line.startswith("G_DIETPI_VERSION_CORE") or line.startswith("G_DIETPI_VERSION_SUB") or line.startswith("G_DIETPI_VERSION_RC"):
-				ver += "%s." % line.split('=')[1].rstrip('\n')
-		ver = ver.rstrip(".")
-		return ver
-	else:
-		return "unknow"
+	return ""
 	
 def hbold(item):
 	return telebot.formatting.hbold(item)
@@ -34,14 +23,14 @@ def hbold(item):
 hostname = hbold(get_str_from_file("/proc/sys/kernel/hostname"))
 current_path = "/root/update_check"
 tmp_file = "/tmp/status_update.tmp"
-mode = mode_command = "silent"
-update_message = upgrade_message = patch_message = ""
+ORANGE_DOT, GREEN_DOT = "\U0001F7E0", "\U0001F7E2"
+file_messages = [['/run/dietpi/.apt_updates', 'apt update(s) available'], ['/run/dietpi/.update_available', 'upgrade available'], ['/run/dietpi/.live_patches', 'live patch(es) available']]
+
 if os.path.exists(f"{current_path}/config.json"):
 	parsed_json = json.loads(open(f"{current_path}/config.json", "r").read())
 	min_repeat = int(parsed_json["minutes"])
 else:
 	min_repeat = 3
-ORANGE_DOT, GREEN_DOT = "\U0001F7E0", "\U0001F7E2"
 
 if os.path.exists(f"{current_path}/telegram_bot.json"):
 	parsed_json = json.loads(open(f"{current_path}/telegram_bot.json", "r").read())
@@ -57,50 +46,32 @@ else:
 
 @repeat(every(min_repeat).minutes)
 def update_check():
-	status_message = old_status_str = new_status_str = ""
+	new_status_str = update_message = ""
+	old_status_str = "000"
+	li = ["0", "0", "0"]
 	if not os.path.exists(tmp_file) or os.path.getsize(tmp_file) != 3:
 		with open(tmp_file, "w") as status_file:
-			for i in range(0, 3):
-				old_status_str += "0"
 			status_file.write(old_status_str)
 		status_file.close()
 	with open(tmp_file, "r") as status_file:
 		old_status_str = status_file.read()
-		li = list(old_status_str)
 		status_file.close()
-	if os.path.exists("/run/dietpi/.apt_updates"):
-		update_message = f"{ORANGE_DOT} - {get_str_from_file('/run/dietpi/.apt_updates')} apt update(s) available"
-		li[0] = "1"
-	else:
-		update_message = f"{GREEN_DOT} - no apt update(s) available"
-		li[0] = "0"
-	if os.path.exists("/run/dietpi/.update_available"):
-		upgrade_message = f"{ORANGE_DOT} - {get_current_version()}->{get_str_from_file('/run/dietpi/.update_available')} upgrade available"
-		li[1] = "1"
-	else:
-		upgrade_message = f"{GREEN_DOT} - no upgrade available"
-		li[1] = "0"
-	if os.path.exists("/run/dietpi/.live_patches"):
-		patch_message = f"{ORANGE_DOT} - {get_str_from_file('/run/dietpi/.live_patches')} live patch(es) available"
-		li[2] = "1"
-	else:
-		patch_message = f"{GREEN_DOT} - no live patch(es) available"
-		li[2] = "0"
+	for i in range(0, 3):
+		if os.path.exists(file_messages[i][0]):
+			update_message += f"{ORANGE_DOT} - {get_str_from_file(file_messages[i][0])} {file_messages[i][1]}\n"
+			li[i] = "1"
+		else:
+			update_message += f"{GREEN_DOT} - no {file_messages[i][1]}\n"
+			li[i] = "0"
 	new_status_str = "".join(li)
-	if old_status_str == new_status_str:
-		mode = "silent"
-	else:
-		mode = "info"
+	if old_status_str != new_status_str:
 		with open(tmp_file, "w") as status_file:
 			status_file.write(new_status_str)
 			status_file.close()
-	if mode_command == "info" or mode == "info":
 		try:
-			tb.send_message(CHAT_ID, f"{hostname} (updates)\n{update_message}\n{upgrade_message}\n{patch_message}\n", parse_mode='html')
+			tb.send_message(CHAT_ID, f"{hostname} (updates)\n{update_message}", parse_mode='html')
 		except Exception as e:
 			print(f"error: {e}")
-	#print(f"{hostname}\n{update_message}\n{upgrade_message}\n{patch_message}")
-
 while True:
     run_pending()
     time.sleep(1)
