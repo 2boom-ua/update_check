1038fa7523a36a578154b6163f6d338ed20b5ba6
Vladimi2boom <97739181+Vladimi2boom@users.noreply.github.com>
2024-03-31 19:54:19 +0300

    Add files via upload

--- update_check.py
+++ update_check.py
@@ -2,10 +2,11 @@
 # -*- coding: utf-8 -*-
 # Copyright (c) 2boom 2023-24
 
-import yaml
+import json
 import os
 import telebot
 import time
+import discord_notify as dn
 from schedule import every, repeat, run_pending
 
 def get_str_from_file(filename : str):
@@ -14,27 +15,41 @@ def get_str_from_file(filename : str):
 		ret = open(filename, 'r').read().strip('\n')
 	return ret
 
-def telegram_message(message : str):
-	try:
-		tb.send_message(CHAT_ID, message, parse_mode='markdown')
-	except Exception as e:
-		print(f"error: {e}")
+def send_message(message : str):
+	if TELEGRAM_ON:
+		try:
+			tb.send_message(CHAT_ID, message, parse_mode="markdown")
+		except Exception as e:
+			print(f"error: {e}")
+	if DISCORD_ON:
+		try:
+			notifier.send(message.replace("*", "**").replace("\t", ""), print_message=False)
+		except Exception as e:
+			print(f"error: {e}")
 
 if __name__ == "__main__":
-	FileNameMessage = [['/run/dietpi/.apt_updates', 'apt update(s) available'], ['/run/dietpi/.update_available', 'upgrade available'], ['/run/dietpi/.live_patches', 'live patch(es) available']]
+	FileNameMessage = [['/run/dietpi/.apt_updates', 'apt update(s) available'], ['/run/dietpi/.update_available', 'upgrade available'],\
+	['/run/dietpi/.live_patches', 'live patch(es) available']]
 	HOSTNAME = open('/proc/sys/kernel/hostname', 'r').read().strip('\n')
 	CURRENT_PATH =  os.path.dirname(os.path.realpath(__file__))
 	TMP_FILE = "/tmp/status_update.tmp"
 	ORANGE_DOT, GREEN_DOT = "\U0001F7E0", "\U0001F7E2"
-	if os.path.exists(f"{CURRENT_PATH}/config.yml"):
-		with open(f"{CURRENT_PATH}/config.yml", 'r') as file:
-			parsed_yaml = yaml.safe_load(file)
-			TOKEN = parsed_yaml["telegram"]["TOKEN"]
-			CHAT_ID = parsed_yaml["telegram"]["CHAT_ID"]
-			MIN_REPEAT = parsed_yaml["timeout"]["MIN_REPEAT"]
-		file.close()
-		tb = telebot.TeleBot(TOKEN)
-		telegram_message(f"*{HOSTNAME}* (updates)\nupgrade, updates, patches monitor started:\n- polling period {MIN_REPEAT} minute(s).")
+	if os.path.exists(f"{CURRENT_PATH}/config.json"):
+		parsed_json = json.loads(open(f"{CURRENT_PATH}/config.json", "r").read())
+		TELEGRAM_ON = parsed_json["TELEGRAM"]["ON"]
+		DISCORD_ON = parsed_json["DISCORD"]["ON"]
+		if TELEGRAM_ON:
+			TOKEN = parsed_json["TELEGRAM"]["TOKEN"]
+			CHAT_ID = parsed_json["TELEGRAM"]["CHAT_ID"]
+			tb = telebot.TeleBot(TOKEN)
+		if DISCORD_ON:
+			DISCORD_WEB = parsed_json["DISCORD"]["WEB"]
+			notifier = dn.Notifier(DISCORD_WEB)
+		MIN_REPEAT = int(parsed_json["MIN_REPEAT"])
+		send_message(f"*{HOSTNAME}* (updates)\nupgrade, updates, patches monitor started:\n\
+		- polling period: {MIN_REPEAT} minute(s),\n\
+		- messenging Telegram: {str(TELEGRAM_ON).lower()},\n\
+		- messenging Discord: {str(DISCORD_ON).lower()}.")
 	else:
 		print("config.yml not found")
 	
@@ -62,7 +77,7 @@ def update_check():
 		with open(TMP_FILE, "w") as file:
 			file.write(NEW_STATUS)
 		file.close()
-		telegram_message(f"*{HOSTNAME}* (updates)\n{MESSAGE}")
+		send_message(f"*{HOSTNAME}* (updates)\n{MESSAGE}")
 
 while True:
     run_pending()
