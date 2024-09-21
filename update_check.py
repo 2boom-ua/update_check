9fcf12610cca340f166e3e41a2154105c52f5cef
2boom <97739181+2boom-ua@users.noreply.github.com>
2024-09-21 17:51:46 +0300

    Update update_check.py

--- update_check.py
+++ update_check.py
@@ -9,7 +9,7 @@ import requests
 from schedule import every, repeat, run_pending
 
 
-def get_string(filename: str) -> str:
+def GetString(filename: str) -> str:
 	"""Return the content of the file as a string"""
 	if os.path.exists(filename):
 		with open(filename, 'r') as file:
@@ -17,7 +17,7 @@ def get_string(filename: str) -> str:
 	return ""
 
 
-def get_hostname() -> str:
+def getHostName() -> str:
 	"""Get the hostname."""
 	hostname = ""
 	hostname_path = '/proc/sys/kernel/hostname'
@@ -27,9 +27,10 @@ def get_hostname() -> str:
 	return hostname
 
 
-def send_message(message: str):
-	"""Send notifications to various messaging services (Telegram, Discord, Slack, Gotify, Ntfy, Pushbullet, Pushover)."""
-	def send_request(url, json_data=None, data=None, headers=None):
+def SendMessage(message: str):
+	"""Send notifications to various messaging services (Telegram, Discord, Slack, Gotify, Ntfy, Pushbullet, Pushover, Matrix)."""
+	def SendRequest(url, json_data=None, data=None, headers=None):
+		"""Send an HTTP POST request and handle exceptions."""
 		try:
 			response = requests.post(url, json=json_data, data=data, headers=headers)
 			response.raise_for_status()
@@ -39,41 +40,50 @@ def send_message(message: str):
 		for token, chat_id in zip(telegram_tokens, telegram_chat_ids):
 			url = f"https://api.telegram.org/bot{token}/sendMessage"
 			json_data = {"chat_id": chat_id, "text": message, "parse_mode": "Markdown"}
-			send_request(url, json_data)
+			SendRequest(url, json_data)
 	if discord_on:
 		for token in discord_tokens:
 			url = f"https://discord.com/api/webhooks/{token}"
 			json_data = {"content": message.replace("*", "**")}
-			send_request(url, json_data)
+			SendRequest(url, json_data)
 	if slack_on:
 		for token in slack_tokens:
 			url = f"https://hooks.slack.com/services/{token}"
 			json_data = {"text": message}
-			send_request(url, json_data)
+			SendRequest(url, json_data)
+	if matrix_on:
+		for token, server_url, room_id in zip(matrix_tokens, matrix_server_urls, matrix_room_ids):
+			url = f"{server_url}/_matrix/client/r0/rooms/{room_id}/send/m.room.message?access_token={token}"
+			matrix_message = "<br>".join(string.replace('*', '<b>', 1).replace('*', '</b>', 1) for string in message.split("\n"))
+			json_data = {"msgtype": "m.text", "body": matrix_message, "format": "org.matrix.custom.html", "formatted_body": matrix_message}
+			headers_data = {"Content-Type": "application/json"}
+			SendRequest(url, json_data, None, headers_data)
+	
 	header, message = message.replace("*", "").split("\n", 1)
 	message = message.strip()
+
 	if gotify_on:
 		for token, chat_url in zip(gotify_tokens, gotify_chat_urls):
 			url = f"{chat_url}/message?token={token}"
 			json_data = {'title': header, 'message': message, 'priority': 0}
-			send_request(url, json_data)
+			SendRequest(url, json_data)
 	if ntfy_on:
 		for token, chat_url in zip(ntfy_tokens, ntfy_chat_urls):
 			url = f"{chat_url}/{token}"
 			data_data = message.encode(encoding = 'utf-8')
 			headers_data = {"title": header}
-			send_request(url, None, data_data, headers_data)
+			SendRequest(url, None, data_data, headers_data)
 	if pushbullet_on:
 		for token in pushbullet_tokens:
 			url = "https://api.pushbullet.com/v2/pushes"
 			json_data = {'type': 'note', 'title': header, 'body': message}
 			headers_data = {'Access-Token': token, 'Content-Type': 'application/json'}
-			send_request(url, json_data, None, headers_data)
+			SendRequest(url, json_data, None, headers_data)
 	if pushover_on:
 		for token, user_key in zip(pushover_tokens, pushover_user_keys):
 			url = "https://api.pushover.net/1/messages.json"
 			json_data = {"token": token, "user": user_key, "message": message, "title": header}
-			send_request(url, json_data)
+			SendRequest(url, json_data)
 
 
 
@@ -84,7 +94,7 @@ if __name__ == "__main__":
 		['/run/dietpi/.update_available', 'upgrade available'],
 		['/run/dietpi/.live_patches', 'live patch(es) available']
 	]
-	hostname = get_hostname()
+	hostname = getHostName()
 	header = f"*{hostname}* (updates)\n"
 	current_path =  os.path.dirname(os.path.realpath(__file__))
 	dots = {"orange": "\U0001F7E0", "green": "\U0001F7E2"}
@@ -97,23 +107,21 @@ if __name__ == "__main__":
 		if not default_dot_style:
 			dots = square_dots
 		orange_dot, green_dot = dots["orange"], dots["green"]
-		telegram_on, discord_on, gotify_on, ntfy_on, pushbullet_on, pushover_on, slack_on = (parsed_json[key]["ON"] for key in ["TELEGRAM", "DISCORD", "GOTIFY", "NTFY", "PUSHBULLET", "PUSHOVER", "SLACK"])
-		services = {
-		"TELEGRAM": ["TOKENS", "CHAT_IDS"], "DISCORD": ["TOKENS"], "SLACK": ["TOKENS"], "GOTIFY": ["TOKENS", "CHAT_URLS"],
-		"NTFY": ["TOKENS", "CHAT_URLS"], "PUSHBULLET": ["TOKENS"], "PUSHOVER": ["TOKENS", "USER_KEYS"]
-		}
+		telegram_on, discord_on, gotify_on, ntfy_on, pushbullet_on, pushover_on, slack_on, matrix_on = (parsed_json[key]["ON"] for key in ["TELEGRAM", "DISCORD", "GOTIFY", "NTFY", "PUSHBULLET", "PUSHOVER", "SLACK", "MATRIX"])
+		services = {"TELEGRAM": ["TOKENS", "CHAT_IDS"], "DISCORD": ["TOKENS"], "SLACK": ["TOKENS"],"GOTIFY": ["TOKENS", "CHAT_URLS"],
+			"NTFY": ["TOKENS", "CHAT_URLS"], "PUSHBULLET": ["TOKENS"], "PUSHOVER": ["TOKENS", "USER_KEYS"], "MATRIX": ["TOKENS", "SERVER_URLS", "ROOM_IDS"]}
 		for service, keys in services.items():
 			if parsed_json[service]["ON"]:
 				globals().update({f"{service.lower()}_{key.lower()}": parsed_json[service][key] for key in keys})
 				monitoring_mg += f"- messaging: {service.capitalize()},\n"
 		min_repeat = int(parsed_json["MIN_REPEAT"])
-		send_message(f"{header}upgrade, updates, patches monitor:\n{monitoring_mg}- polling period: {min_repeat} minute(s).")
+		SendMessage(f"{header}upgrade, updates, patches monitor:\n{monitoring_mg}- polling period: {min_repeat} minute(s).")
 	else:
 		print("config.json not found")
 
 
 @repeat(every(min_repeat).minutes)
-def update_check():
+def UpdateCheck():
 	"""Periodically check for updates, upgrade and patch aviable"""
 	new_status = message =""
 	current_status = []
@@ -123,7 +131,7 @@ def update_check():
 	for i, (file_path, description) in enumerate(update_status_files):
 		if os.path.exists(file_path):
 			if old_status[i] == "0":
-				message += f"{orange_dot} {get_string(file_path)} {description}\n"
+				message += f"{orange_dot} {GetString(file_path)} {description}\n"
 			current_status[i] = "1"
 		else:
 			if old_status[i] == "1":
@@ -132,7 +140,7 @@ def update_check():
 	new_status = "".join(current_status)
 	if old_status != new_status:
 		old_status = new_status
-		send_message(f"{header}{message}")
+		SendMessage(f"{header}{message}")
 
 while True:
 	run_pending()
